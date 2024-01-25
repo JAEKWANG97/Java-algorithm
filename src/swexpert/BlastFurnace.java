7aa98117b45fcd5fbaa88227ae44c8240c9b8cbf
Picka <marmong@naver.com>
2024-01-25 20:05:15 +0900

    Create BlastFurnace.java

---
+++ src/swexpert/BlastFurnace.java
@@ -0,0 +1,29 @@
+package swexpert;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BlastFurnace {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int k = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[k];
+        int minValue = Math.abs(n - m);
+        for (int i = 0; i < k; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+            if(arr[i] - m < minValue){
+                minValue = arr[i] - m + 1;
+            }
+        }
+        System.out.println(minValue);
+
+    }
+}
